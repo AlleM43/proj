388b5f3f40b6a511f7680c150c5ccba4bc9d5c91
AlleM43 <albinbalbinbus@hotmail.se>
2015-12-02 15:31:52 +0100

    well

---
+++ fibbonaci/src/fibbonaci/feebles.java
@@ -0,0 +1,41 @@
+package fibbonaci;
+import javax.swing.*;
+public class feebles {
+
+	public static void main(String[] args) {		
+	int a=1;
+	int b=1;
+	int c=0;
+	
+	
+	
+	String out="";{
+	String timesstr = JOptionPane.showInputDialog("Önskat nummer");
+	int times = Integer.parseInt(timesstr);
+	
+		while ( times >1)
+			{
+			if (times>47){
+				System.out.println ("CRASH PREVENTION SYSTEM ACTIVATED");
+				JOptionPane.showMessageDialog(null,"CRASH PREVENTION SYSTEM ACTIVATED" );
+				System.exit(0);
+			}
+			c=a+b;
+			a=b;
+			b=c;
+			c=0;
+			times=times-1;
+			}
+		
+			out="" + b;
+			System.out.println(out);
+			JOptionPane.showMessageDialog(null,out);
+					
+		}
+	}	
+
+		// TODO Auto-generated method stub
+
+	}
+
+
